aed04e58320b0e9246a8e8fb5e9f0e4fb00425b1
michaeltryby <michael.tryby@gmail.com>
2025-08-20 21:22:58 -0400

    Fussing

--- cmake/FindBoostTestHeaders.cmake
+++ cmake/FindBoostTestHeaders.cmake
@@ -1,4 +1,22 @@
-
+#
+#  FindBoostTestHeaders.cmake
+#
+#  Purpose:
+#    CMake module to fetch and configure Boost header-only libraries
+#    required for Boost.Test, using FetchContent. Creates an INTERFACE
+#    target 'boost_test_headers' with all necessary include paths.
+#
+#  Usage:
+#    include(cmake/FindBoostTestHeaders.cmake)
+#    target_link_libraries(<your_target> PRIVATE boost_test_headers)
+#
+#  Repository:
+#    https://github.com/michaeltryby/boost-test-headers
+#
+#  Copyright (c) 2025 Michael E. Tryby and contributors
+#
+#  License: MIT
+#
 
 cmake_policy(SET CMP0169 OLD)
 
@@ -15,37 +33,37 @@ set(
 )
 
 set(
-  BOOST_LIBRARIES
-    algorithm
-    assert
-    range
-    config
-    core
-    move
-    numeric_conversion
-    static_assert
-    throw_exception
-    type_traits
-    preprocessor
-    mpl
-    mp11
-    utility
-    io
-    function
-    bind
-    smart_ptr
-    exception
-    detail
-    iterator
-    test
+  BOOST_TEST_DEPS
+  algorithm
+  assert
+  bind
+  config
+  core
+  detail
+  exception
+  function
+  io
+  iterator
+  move
+  mp11
+  mpl
+  numeric_conversion
+  preprocessor
+  range
+  smart_ptr
+  static_assert
+  test
+  throw_exception
+  type_traits
+  utility
 )
 
-foreach(lib IN LISTS BOOST_LIBRARIES)
+foreach(lib IN LISTS BOOST_TEST_DEPS)
     FetchContent_Declare(
       Boost${lib}
         URL https://github.com/boostorg/${lib}/archive/refs/tags/boost-${BOOST_VERSION}.tar.gz
     )
-    # TODO: To be deprecated (See CMP0169). Find work around
+    # TODO: To be deprecated (See CMP0169). Need work around
     FetchContent_Populate(
       Boost${lib}
     )
@@ -58,7 +76,7 @@ add_library(
     INTERFACE
 )
 
-foreach(lib IN LISTS BOOST_LIBRARIES)
+foreach(lib IN LISTS BOOST_TEST_DEPS)
     target_include_directories(
       boost_test_headers
         INTERFACE
@@ -66,4 +84,6 @@ foreach(lib IN LISTS BOOST_LIBRARIES)
 endforeach()
 
 # Mark as found
-set(boost_test_headers_FOUND TRUE)
+set(
+    boost_test_headers_FOUND TRUE
+)
